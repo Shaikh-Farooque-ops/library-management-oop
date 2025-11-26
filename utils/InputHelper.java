bf8e95010babd88b34b8cdb8ae7802f40a24da8b
Farooque <farooqueshaikh7499@gmail.com>
2025-11-26 17:40:35 +0530

    add InputHelper

--- utils/InputHelper.java
+++ utils/InputHelper.java
@@ -1,5 +1,31 @@
 package com.library.utils;
 
-public class InputHelper {
+import java.util.Scanner;
 
+public class InputHelper {
+	
+	private static final Scanner sc = new Scanner(System.in);
+
+	public static String readString(String message)
+	{
+		System.out.print(message);
+		return sc.nextLine();
+	}
+	
+	public static int readInt(String message)
+	{
+		System.out.print(message);
+		
+		while(true)
+		{
+			try
+			{
+				return Integer.parseInt(sc.nextLine());
+			}
+			catch(NumberFormatException  e)
+			{
+				System.out.print("Invalid number. Try again: ");
+			}
+		}
+	}
 }
