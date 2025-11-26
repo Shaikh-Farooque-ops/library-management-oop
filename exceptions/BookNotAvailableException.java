ca68aee88e20af67cbb1d1d15f1b7f09bd119f03
Farooque <farooqueshaikh7499@gmail.com>
2025-11-26 11:53:29 +0530

    add BookNotAvailableException

--- exceptions/BookNotAvailableException.java
+++ exceptions/BookNotAvailableException.java
@@ -1,5 +1,11 @@
 package com.library.exceptions;
 
-public class BookNotAvailableException {
+public class BookNotAvailableException extends Exception{
 
+	private static final long serialVersionUID = 1L;
+
+	public BookNotAvailableException(String message) {
+		super(message);
+	}
+	
 }
